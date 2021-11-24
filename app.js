0bc0c4713bf55f083291fd3c05b9df7b310f828e
Alex Stoliar <alexstolr@gmail.com>
2021-11-24 21:13:36 +0200

    update parsed fields

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var fs = require('fs'); 
 const axios = require('axios').default;
+const bearerToken = 'YOUR_TOKEN';
 
 const get = async function(route){
-        return axios.get(route, { headers: {'Authorization': `Bearer YOUR_TOKEN`}});
+        return axios.get(route, { headers: {'Authorization': `Bearer ${bearerToken}`}});
 }
 
 const exec = async function(promise){
@@ -16,27 +17,26 @@ const exec = async function(promise){
 }
 
 const parseMessage = function(message){
-    let parsedText = '';
-    const {id, sent, text, fromUser} = message;
-    if(text.startsWith('0x')){
-        const {url} = fromUser
-        if(!url) return `${id},${sent},${text},\n`
-        let userAndSource = url.split('_')
-        if(!userAndSource[1]) userAndSource[1] = 'github'
-        userAndSource[0] = userAndSource[0].substring(1)
-        parsedText += `${id},${sent},${text},${userAndSource[0]},${userAndSource[1]}\n`
-        console.log(`id: ${id} sent: ${sent} address: ${text} username: ${userAndSource[0]} source: ${userAndSource[1]}`)
+        let parsedText = '';
+        const {id, text, html, sent, unread, readby, urls, mentions, issues, meta, v, fromUser} = message;
+        const {username, displayName, url, avatarUrl, avatarUrlSmall, avatarUrlMedium} = fromUser;
+
+        if(text.startsWith('0x')){
+            let address = text.substring(0, 42);
+            address = address.toLowerCase();
+            let userAndSource = username.split('_');
+            if(!userAndSource[1]) userAndSource[1] = 'github';
+            parsedText += `${sent},${id},${address},${userAndSource[0]},${displayName},${avatarUrlMedium},${userAndSource[1]}\n`;
+        }
+        return parsedText;
     }
-    return parsedText
-}
 
 const scrape = async function(){
     let hasMore = true;
     let lastId = null; //'603e2548e8267a46f2e8824a';
     const route = 'https://api.gitter.im/v1/rooms/58ba6dc5d73408ce4f4e4d68/chatMessages?limit=50'
-    fs.appendFile('data.txt', 'id,sent,address,username,source\n', function (err) {
+    fs.appendFile('data.txt', 'sent,id,address,username,display_name,avataUrl,source\n', function (err) { 
         if (err) throw err;
-        console.log('Updated!');
     }); 
     while(hasMore){
         if(lastId !== null){
@@ -49,7 +49,6 @@ const scrape = async function(){
             if(gitterMessages.status === 200){
                 let data = gitterMessages.data
                 let text = '';
-                console.log(`data.length: ${data.length}`)
                 if(data.length < 50){
                     hasMore = false;
                 }
